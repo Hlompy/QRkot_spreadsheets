3931e719fe6d3a18c19cb44360ad3cab7066fd63
Hlompy <K.viregor@gmail.com>
2023-01-26 15:32:55 +0000

    Before review

---
+++ app/crud/charity_project.py
@@ -0,0 +1,51 @@
+from typing import Optional, Union
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.crud.base import CRUDBase
+from app.models.charity_project import CharityProject
+
+
+class CRUDCharity(CRUDBase):
+
+    async def get_id_by_name(
+        self,
+        project_name: str,
+        session: AsyncSession,
+    ) -> Optional[int]:
+        db_project_id = await session.execute(
+            select(CharityProject.id).where(
+                CharityProject.name == project_name
+            )
+        )
+        db_project_id = db_project_id.scalars().first()
+        return db_project_id
+
+    async def get_project_by_id(
+        self,
+        project_id: int,
+        session: AsyncSession,
+    ) -> Optional[CharityProject]:
+        db_project = await session.execute(
+            select(CharityProject).where(
+                CharityProject.id == project_id
+            )
+        )
+        db_project = db_project.scalars().first()
+        return db_project
+
+    async def get_projects_by_completion_rate(
+        self,
+        session: AsyncSession,
+    ) -> list[dict[str, Union[str, int]]]:
+        projects = await session.execute(
+            select(CharityProject).where(
+                CharityProject.fully_invested == 1
+            )
+        )
+        projects = projects.scalars().all()
+        return projects
+
+
+project_crud = CRUDCharity(CharityProject)
